055c6cd32c33a6b3ff1ecbeb7ad9d54a5b801689
IngeniozIT <contact@ingenioz.it>
2023-04-19 22:55:18 +0200

    Add UploadedFile

---
+++ src/UploadedFile.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IngeniozIT\Http\Message;
+
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use RuntimeException;
+
+class UploadedFile implements UploadedFileInterface
+{
+    private bool $moved = false;
+
+    public function __construct(
+        private readonly StreamInterface $stream,
+        private readonly StreamFactoryInterface $streamFactory,
+        private readonly ?int $size = null,
+        private readonly int $error = UPLOAD_ERR_OK,
+        private readonly ?string $clientFilename = null,
+        private readonly ?string $clientMediaType = null,
+    ) {
+    }
+
+    public function getStream(): StreamInterface
+    {
+        $this->assertNotMoved();
+        return $this->stream;
+    }
+
+    public function moveTo($targetPath): void
+    {
+        $this->assertNotMoved();
+        $this->moveStream($targetPath);
+        $this->closeStream();
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function getError(): int
+    {
+        return $this->error;
+    }
+
+    public function getClientFilename(): ?string
+    {
+        return $this->clientFilename;
+    }
+
+    public function getClientMediaType(): ?string
+    {
+        return $this->clientMediaType;
+    }
+
+    private function assertNotMoved(): void
+    {
+        if ($this->moved) {
+            throw new RuntimeException('Stream has been moved');
+        }
+    }
+
+    private function moveStream(string $targetPath): void
+    {
+        $newStream = $this->streamFactory->createStreamFromFile($targetPath, 'w+');
+        $newStream->write((string)$this->stream);
+        $this->moved = true;
+    }
+
+    private function closeStream(): void
+    {
+        $previousFile = $this->stream->getMetadata('uri');
+        if (is_string($previousFile) && file_exists($previousFile)) {
+            unlink($previousFile);
+        }
+        $this->stream->close();
+    }
+}
---
+++ tests/UploadedFileTest.php
@@ -0,0 +1,180 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IngeniozIT\Http\Message\Tests;
+
+use IngeniozIT\Http\Message\{StreamFactory, UploadedFile};
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\{StreamFactoryInterface, StreamInterface, UploadedFileInterface,};
+use RuntimeException;
+
+class UploadedFileTest extends TestCase
+{
+    use WithTempFiles;
+
+    public function testIsAPsrUploadedFile(): void
+    {
+        $uploadedFile = $this->createSubject();
+
+        self::assertInstanceOf(UploadedFileInterface::class, $uploadedFile);
+    }
+
+    public function testHasADataStream(): void
+    {
+        $stream = $this->getStream();
+        $uploadedFile = $this->createSubject(stream: $stream);
+
+        $fileStream = $uploadedFile->getStream();
+
+        self::assertSame($stream, $fileStream);
+    }
+
+    public function testStreamCanBeMovedToAnotherLocation(): void
+    {
+        $stream = $this->getStream();
+        $streamContent = (string) $stream;
+        $uploadedFile = $this->createSubject(
+            stream: $stream
+        );
+
+        $uploadedFile->moveTo(self::$availableFile);
+        $fileContent = file_get_contents(self::$availableFile);
+
+        self::assertEquals($streamContent, $fileContent);
+    }
+
+    public function testOriginalStreamIsRemovedOnCompletion(): void
+    {
+        $factory = new StreamFactory();
+        $stream = $factory->createStreamFromFile(self::$file);
+        $uploadedFile = $this->createSubject(
+            stream: $stream
+        );
+
+        $uploadedFile->moveTo(self::$availableFile);
+
+        self::assertFileDoesNotExist(self::$file);
+        self::assertNull($stream->detach());
+        self::expectException(RuntimeException::class);
+        $uploadedFile->getStream();
+    }
+
+    public function testStreamCannotBeMovedToAnInvalidPath(): void
+    {
+        $uploadedFile = $this->createSubject();
+
+        self::expectException(RuntimeException::class);
+        $uploadedFile->moveTo('/');
+    }
+
+    public function testStreamCannotBeMovedWhenItHasAlreadyBeenMoved(): void
+    {
+        $uploadedFile = $this->createSubject();
+        $uploadedFile->moveTo(self::$availableFile);
+
+        self::expectException(RuntimeException::class);
+        $uploadedFile->moveTo(self::$availableFile);
+    }
+
+    public function testCanHaveASize(): void
+    {
+        $uploadedFile = $this->createSubject(
+            size: 42,
+        );
+
+        $size = $uploadedFile->getSize();
+
+        self::assertEquals(42, $size);
+    }
+
+    public function testSizeCanBeNull(): void
+    {
+        $uploadedFile = $this->createSubject(
+            size: null,
+        );
+
+        $size = $uploadedFile->getSize();
+
+        self::assertNull($size);
+    }
+
+    public function testHasAnErrorCode(): void
+    {
+        $uploadedFile = $this->createSubject(
+            error: UPLOAD_ERR_PARTIAL,
+        );
+
+        $error = $uploadedFile->getError();
+
+        self::assertEquals(UPLOAD_ERR_PARTIAL, $error);
+    }
+
+    public function testCanHaveAClientFilename(): void
+    {
+        $uploadedFile = $this->createSubject(
+            clientFilename: 'foo.bar',
+        );
+
+        $filename = $uploadedFile->getClientFilename();
+
+        self::assertEquals('foo.bar', $filename);
+    }
+
+    public function testClientFilenameCanBeNull(): void
+    {
+        $uploadedFile = $this->createSubject(
+            clientFilename: null,
+        );
+
+        $filename = $uploadedFile->getClientFilename();
+
+        self::assertNull($filename);
+    }
+
+    public function testCanHaveAClientMediaType(): void
+    {
+        $uploadedFile = $this->createSubject(
+            clientMediaType: 'foo.bar',
+        );
+
+        $mediaType = $uploadedFile->getClientMediaType();
+
+        self::assertEquals('foo.bar', $mediaType);
+    }
+
+    public function testClientFileTypeCanBeNull(): void
+    {
+        $uploadedFile = $this->createSubject(
+            clientMediaType: null,
+        );
+
+        $mediaType = $uploadedFile->getClientMediaType();
+
+        self::assertNull($mediaType);
+    }
+
+    protected function getStream(): StreamInterface
+    {
+        $factory = new StreamFactory();
+        return $factory->createStream('12345678');
+    }
+
+    public function createSubject(
+        ?StreamInterface $stream = null,
+        ?StreamFactoryInterface $streamFactory = null,
+        ?int $size = null,
+        int $error = UPLOAD_ERR_OK,
+        ?string $clientFilename = null,
+        ?string $clientMediaType = null,
+    ): UploadedFileInterface {
+        return new UploadedFile(
+            stream: $stream ?? $this->getStream(),
+            streamFactory: $streamFactory ?? new StreamFactory(),
+            size: $size ?? null,
+            error: $error,
+            clientFilename: $clientFilename ?? null,
+            clientMediaType: $clientMediaType ?? null,
+        );
+    }
+}
